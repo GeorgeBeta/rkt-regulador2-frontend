86483f0869720f4a9b6c81ebd5eb4e949999b5f9
GeorgeBeta <jorge@zaporta.com>
2025-10-10 11:15:26 +0200

    type your environment variables with env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,18 @@
 /// <reference path="../.astro/types.d.ts" />
+
+interface ImportMetaEnv {
+    readonly PUBLIC_IS_MOCK: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
+
+declare namespace App {
+    interface Locals {
+        user?: {
+            email: string;
+            name:string;
+        }
+    }
+}
